1ab0fc10e300dfed3accaf5ea19dcb72301fd476
Cristina Souza <cristinars@dcc.ufrj.br>
2025-06-19 20:25:16 -0300

    feat: integrate Vercel Analytics and update icon library to MUI

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -2,6 +2,7 @@ import { EnvVarWarning } from '@/components/env-var-warning'
 import { AuthButton } from '@/components/auth-button'
 import { ThemeSwitcher } from '@/components/theme-switcher'
 import { hasEnvVars } from '@/lib/utils'
+import { Analytics } from '@vercel/analytics/next'
 import Link from 'next/link'
 import EmailIcon from '@mui/icons-material/Email'
 import InstagramIcon from '@mui/icons-material/Instagram'
@@ -13,6 +14,7 @@ export default function ProtectedLayout({
 }) {
     return (
         <main className="min-h-screen flex flex-col items-center">
+            <Analytics />
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
                 <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                     <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
