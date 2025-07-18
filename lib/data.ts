79e310af246b449ea694d9b41f75706d0d232601
Cristina Souza <cristinars@dcc.ufrj.br>
2025-07-18 14:53:55 -0300

    refac: update ESLint rules and add directions for data definitions

---
+++ lib/data.ts
@@ -0,0 +1,56 @@
+import { createClient } from '@supabase/supabase-js'
+
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
+
+// ---- EXAMPLES ---- //
+// export async function fetchRevenue() {
+//     const { data, error } = await supabase.from('revenue').select('*')
+
+//     if (error) {
+//         console.error('Database Error:', error)
+//         throw new Error('Failed to fetch revenue data.')
+//     }
+
+//     return data
+// }
+
+// export async function fetchLatestInvoices() {
+//     const { data, error } = await supabase
+//         .from('invoices')
+//         .select('amount, id, customers(name, image_url, email)')
+//         .order('date', { ascending: false })
+//         .limit(5)
+
+//     if (error) {
+//         console.error('Database Error:', error)
+//         throw new Error('Failed to fetch the latest invoices.')
+//     }
+
+//     return data.map((invoice) => ({
+//         ...invoice,
+//         amount: formatCurrency(invoice.amount),
+//     }))
+// }
+
+
+
+
+
+// TODO: fetchUserDataById
+
+// TODO: fetchCourseList
+
+// TODO: fetchCourseByUser
+
+// TODO: fetchCourseByProfessor
+
+// TODO: fetchCourseById
+
+// TODO: fetchContentByCourse
+
+// TODO: come up with whatever else
---
+++ lib/models.ts
@@ -0,0 +1,8 @@
+// This file contains type definitions for your data.
+// If unsure, ask Copilot or GPT to define the types for you ;)
+export type User = {
+    id: string
+    name: string
+    email: string
+    password: string
+}
