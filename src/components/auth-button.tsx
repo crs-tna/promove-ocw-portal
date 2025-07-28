78b9b24310d4e1d6b5843d71b34e3cf9ef617342
Cristina Souza <cristinars@dcc.ufrj.br>
2025-07-28 20:03:08 -0300

    refac: updating file structure

---
+++ src/components/auth-button.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import { Button } from './ui/button'
+import { createClient } from '@/src/common/lib/supabase/server'
+import { LogoutButton } from './logout-button'
+
+export async function AuthButton() {
+    const supabase = await createClient()
+
+    const {
+        data: { user },
+    } = await supabase.auth.getUser()
+
+    return user ? (
+        <div className="flex items-center gap-4">
+            Olá, {user.email}!
+            <LogoutButton />
+        </div>
+    ) : (
+        <div className="flex gap-2">
+            <Button asChild size="sm" variant={'outline'}>
+                <Link href="/auth/login">Entrar</Link>
+            </Button>
+            <Button asChild size="sm" variant={'default'}>
+                <Link href="/auth/sign-up">Criar nova conta</Link>
+            </Button>
+        </div>
+    )
+}
