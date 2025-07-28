f3f292a412fb3d9e7ca096c344a2a1a882589c75
Cristina Souza <cristinars@dcc.ufrj.br>
2025-07-28 20:03:08 -0300

    refac: updating file structure

---
+++ src/components/theme-switcher.tsx
@@ -0,0 +1,95 @@
+'use client'
+
+import { Button } from '@/src/components/ui/button'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+    DropdownMenuTrigger,
+} from '@/src/components/ui/dropdown-menu'
+import LaptopOutlinedIcon from '@mui/icons-material/LaptopOutlined'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined'
+import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
+
+const ThemeSwitcher = () => {
+    const [mounted, setMounted] = useState(false)
+    const { theme, setTheme } = useTheme()
+
+    // useEffect only runs on the client, so now we can safely show the UI
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) {
+        return null
+    }
+
+    const ICON_SIZE = 16
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" size={'sm'}>
+                    {theme === 'light' ? (
+                        <LightModeOutlinedIcon
+                            key="light"
+                            sx={{ fontSize: ICON_SIZE }}
+                            className={'text-muted-foreground'}
+                        />
+                    ) : theme === 'dark' ? (
+                        <DarkModeOutlinedIcon
+                            key="dark"
+                            sx={{ fontSize: ICON_SIZE }}
+                            className={'text-muted-foreground'}
+                        />
+                    ) : (
+                        <LaptopOutlinedIcon
+                            key="system"
+                            sx={{ fontSize: ICON_SIZE }}
+                            className={'text-muted-foreground'}
+                        />
+                    )}
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-content" align="start">
+                <DropdownMenuRadioGroup
+                    value={theme}
+                    onValueChange={(e) => setTheme(e)}
+                >
+                    <DropdownMenuRadioItem className="flex gap-2" value="light">
+                        <LightModeOutlinedIcon
+                            key="light"
+                            sx={{ fontSize: ICON_SIZE }}
+                            className={'text-muted-foreground'}
+                        />{' '}
+                        <span>Claro</span>
+                    </DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem className="flex gap-2" value="dark">
+                        <DarkModeOutlinedIcon
+                            key="dark"
+                            sx={{ fontSize: ICON_SIZE }}
+                            className={'text-muted-foreground'}
+                        />{' '}
+                        <span>Escuro</span>
+                    </DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem
+                        className="flex gap-2"
+                        value="system"
+                    >
+                        <LaptopOutlinedIcon
+                            key="system"
+                            sx={{ fontSize: ICON_SIZE }}
+                            className={'text-muted-foreground'}
+                        />{' '}
+                        <span>Sistema</span>
+                    </DropdownMenuRadioItem>
+                </DropdownMenuRadioGroup>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
+
+export { ThemeSwitcher }
---
+++ src/components/update-password-form.tsx
@@ -0,0 +1,92 @@
+'use client'
+
+import { cn } from '@/src/common/lib/utils'
+import { createClient } from '@/src/common/lib/supabase/client'
+import { Button } from '@/src/components/ui/button'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from '@/src/components/ui/card'
+import { Input } from '@/src/components/ui/input'
+import { Label } from '@/src/components/ui/label'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+
+export function UpdatePasswordForm({
+    className,
+    ...props
+}: React.ComponentPropsWithoutRef<'div'>) {
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState<string | null>(null)
+    const [isLoading, setIsLoading] = useState(false)
+    const router = useRouter()
+
+    const handleForgotPassword = async (e: React.FormEvent) => {
+        e.preventDefault()
+        const supabase = createClient()
+        setIsLoading(true)
+        setError(null)
+
+        try {
+            const { error } = await supabase.auth.updateUser({ password })
+            if (error) throw error
+            // Update this route to redirect to an authenticated route. The user already has an active session.
+            router.push('/protected')
+        } catch (error: unknown) {
+            setError(
+                error instanceof Error ? error.message : 'An error occurred'
+            )
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    return (
+        <div className={cn('flex flex-col gap-6', className)} {...props}>
+            <Card>
+                <CardHeader>
+                    <CardTitle className="text-2xl">
+                        Atualize sua senha.{' '}
+                    </CardTitle>
+                    <CardDescription>
+                        Por favor digite sua nova senha abaixo.{' '}
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form onSubmit={handleForgotPassword}>
+                        <div className="flex flex-col gap-6">
+                            <div className="grid gap-2">
+                                <Label htmlFor="password">Nova senha</Label>
+                                <Input
+                                    id="password"
+                                    type="password"
+                                    placeholder="New password"
+                                    required
+                                    value={password}
+                                    onChange={(e) =>
+                                        setPassword(e.target.value)
+                                    }
+                                />
+                            </div>
+                            {error && (
+                                <p className="text-sm text-red-500">{error}</p>
+                            )}
+                            <Button
+                                type="submit"
+                                className="w-full"
+                                disabled={isLoading}
+                            >
+                                {isLoading
+                                    ? 'Salvando...'
+                                    : 'Salvar nova senha'}
+                            </Button>
+                        </div>
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
