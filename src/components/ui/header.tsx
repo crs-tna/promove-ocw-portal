e70323338bbf013bede9279d2b669a34fde277e1
Cristina Souza <cristinars@dcc.ufrj.br>
2025-07-28 23:02:30 -0300

    feat: replace hero image and update layout

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -3,6 +3,7 @@ import { ThemeSwitcher } from '../theme-switcher'
 import { EnvVarWarning } from '../env-var-warning'
 import { AuthButton } from '../auth-button'
 import { hasEnvVars } from '@/src/common/lib/utils'
+import { PromoveLogo } from '../promove-logo'
 
 export default function Header() {
     return (
@@ -10,7 +11,9 @@ export default function Header() {
             <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                     <div className="flex gap-5 items-center font-semibold">
-                        <Link href={'/'}>Cursos abertos UFRJ</Link>
+                        <Link href={'/'}>
+                            <PromoveLogo />
+                        </Link>
                     </div>
                     <div className="flex gap-5 items-center font-semibold">
                         <ThemeSwitcher />
