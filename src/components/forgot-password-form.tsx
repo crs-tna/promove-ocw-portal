ea01d8165e3ed83a97de3aaa4462a257f3de424b
Cristina Souza <cristinars@dcc.ufrj.br>
2025-07-29 15:47:40 -0300

    🪛[PROD] Updating file structure (#18)
    * refac: updating file structure
    
    * style: format imports for better readability

---
+++ src/components/forgot-password-form.tsx
@@ -0,0 +1,126 @@
+'use client'
+
+import { cn } from '@/src/common/lib/utils'
+import { createClient } from '@/src/common/lib/supabase/client'
+import { Button } from '@/src/components/ui/button'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from '@/src/components/ui/card'
+import { Input } from '@/src/components/ui/input'
+import { Label } from '@/src/components/ui/label'
+import Link from 'next/link'
+import { useState } from 'react'
+
+export function ForgotPasswordForm({
+    className,
+    ...props
+}: React.ComponentPropsWithoutRef<'div'>) {
+    const [email, setEmail] = useState('')
+    const [error, setError] = useState<string | null>(null)
+    const [success, setSuccess] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
+
+    const handleForgotPassword = async (e: React.FormEvent) => {
+        e.preventDefault()
+        const supabase = createClient()
+        setIsLoading(true)
+        setError(null)
+
+        try {
+            // The url which will be included in the email. This URL needs to be configured in your redirect URLs in the Supabase dashboard at https://supabase.com/dashboard/project/_/auth/url-configuration
+            const { error } = await supabase.auth.resetPasswordForEmail(email, {
+                redirectTo: `${window.location.origin}/auth/update-password`,
+            })
+            if (error) throw error
+            setSuccess(true)
+        } catch (error: unknown) {
+            setError(
+                error instanceof Error ? error.message : 'An error occurred'
+            )
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    return (
+        <div className={cn('flex flex-col gap-6', className)} {...props}>
+            {success ? (
+                <Card>
+                    <CardHeader>
+                        <CardTitle className="text-2xl">
+                            Verifique seu e-mail
+                        </CardTitle>
+                        <CardDescription>
+                            Instruções para troca de senha enviadas!
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <p className="text-sm text-muted-foreground">
+                            Se você se registrou usando e-mail e senha, você
+                            receberá um e-mail com instruções para troca da
+                            senha.
+                        </p>
+                    </CardContent>
+                </Card>
+            ) : (
+                <Card>
+                    <CardHeader>
+                        <CardTitle className="text-2xl">
+                            Altere sua senha{' '}
+                        </CardTitle>
+                        <CardDescription>
+                            Digite seu e-mail e nós enviaremos um link para
+                            alteração de senha
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <form onSubmit={handleForgotPassword}>
+                            <div className="flex flex-col gap-6">
+                                <div className="grid gap-2">
+                                    <Label htmlFor="email">E-mail</Label>
+                                    <Input
+                                        id="email"
+                                        type="email"
+                                        placeholder="m@example.com"
+                                        required
+                                        value={email}
+                                        onChange={(e) =>
+                                            setEmail(e.target.value)
+                                        }
+                                    />
+                                </div>
+                                {error && (
+                                    <p className="text-sm text-red-500">
+                                        {error}
+                                    </p>
+                                )}
+                                <Button
+                                    type="submit"
+                                    className="w-full"
+                                    disabled={isLoading}
+                                >
+                                    {isLoading
+                                        ? 'Sending...'
+                                        : 'Send reset email'}
+                                </Button>
+                            </div>
+                            <div className="mt-4 text-center text-sm">
+                                Já possui uma conta??{' '}
+                                <Link
+                                    href="/auth/login"
+                                    className="underline underline-offset-4"
+                                >
+                                    Entrar
+                                </Link>
+                            </div>
+                        </form>
+                    </CardContent>
+                </Card>
+            )}
+        </div>
+    )
+}
