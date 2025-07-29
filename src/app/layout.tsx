b9ea091f415bc80be45443877e13da0910063e14
Cristina Souza <cristinars@dcc.ufrj.br>
2025-07-29 15:40:06 -0300

    feat: implement homepage layout and update hero component with new image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,20 +43,17 @@ export default function RootLayout({
             lang="pt-BR"
             className={`${readexPro.variable} ${roboto.variable} ${montserrat.variable}`}
         >
-            <Analytics />
-
             <body className="font-body antialiased">
+                <Analytics />
                 <ThemeProvider
                     attribute="class"
                     defaultTheme="system"
                     enableSystem
                     disableTransitionOnChange
                 >
-                    <div>
+                    <div className="min-h-screen flex flex-col">
                         <Header />
-                    </div>
-                    <div className="flex justify-center">
-                        <main className="flex justify-center">{children}</main>
+                        <main className="flex-1 w-full">{children}</main>
                     </div>
                 </ThemeProvider>
             </body>
