fa915810544770f78860c4f4479602a746670dee
Francisco Augusto <francisco.acs08@gmail.com>
2025-08-18 13:49:25 -0300

    feat: integrated courses screen

--- src/app/(overview)/page.tsx
+++ src/app/(overview)/page.tsx
@@ -1,12 +1,9 @@
 import { Hero } from '@/src/components/hero'
 
-import Bot from '../../components/bot'
-
 // TODO: implement general homepage (for non-logged in users)
 export default function Home() {
     return (
         <main className="flex flex-col items-center gap-20">
-            <Bot />
             <Hero />
         </main>
     )
