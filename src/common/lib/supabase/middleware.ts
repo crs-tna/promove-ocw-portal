15484eb7d35e519d79ca3db7c318141b2e91ed0a
HenriChris <127856850+HenriChris@users.noreply.github.com>
2025-09-11 18:07:31 -0300

    feat: middleware with role-based access (#26)

--- src/common/lib/supabase/middleware.ts
+++ src/common/lib/supabase/middleware.ts
@@ -1,16 +1,47 @@
 import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
-import { hasEnvVars } from '../../lib/utils'
+
+// TODO provisório, deve-se definir rotas, papéis e permissões de maneira exata e completa
+
+enum Roles {
+    Teacher = 1,
+    Student = 2,
+    Guest = 3,
+}
+
+type RestrictedRoutes = {
+    [route: string]: number[]
+}
+
+interface PermissionsConfig {
+    restrictedRoutes: RestrictedRoutes
+    publicRoutes: string[]
+    authRoutes: string[]
+    redirects: {
+        authenticated: string
+        unauthenticated: string
+        unauthorized: string
+    }
+}
+
+const PERMISSIONS_CONFIG: PermissionsConfig = {
+    restrictedRoutes: {
+        '/user': [Roles.Teacher, Roles.Student],
+        '/protected': [Roles.Teacher],
+    },
+    publicRoutes: ['/unauthorized'],
+    authRoutes: ['/auth'],
+    redirects: {
+        authenticated: '/user/home',
+        unauthenticated: '/auth/login',
+        unauthorized: '/unauthorized',
+    },
+}
 
 export async function updateSession(request: NextRequest) {
     let supabaseResponse = NextResponse.next({
         request,
     })
-
-    // If the env vars are not set, skip middleware check. You can remove this once you setup the project.
-    if (!hasEnvVars) {
-        return supabaseResponse
-    }
 
     const supabase = createServerClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -45,16 +76,72 @@ export async function updateSession(request: NextRequest) {
         data: { user },
     } = await supabase.auth.getUser()
 
-    if (
-        request.nextUrl.pathname !== '/' &&
-        !user &&
-        !request.nextUrl.pathname.startsWith('/login') &&
-        !request.nextUrl.pathname.startsWith('/auth')
-    ) {
-        // no user, potentially respond by redirecting the user to the login page
+    const pathname = request.nextUrl.pathname
+
+    function matchesRoute(routes: string[]): boolean {
+        return routes.some((route) => {
+            return pathname === route || pathname.startsWith(`${route}/`)
+        })
+    }
+
+    function redirect(target: string, redirectTo?: string) {
         const url = request.nextUrl.clone()
-        url.pathname = '/auth/login'
+        url.pathname = target
+        if (redirectTo) url.searchParams.set('redirectTo', redirectTo) // Redireciona para a rota original após redirecionamento
         return NextResponse.redirect(url)
+    }
+
+    // Hero apenas para usuários não logados
+    if (pathname === '/') {
+        return user
+            ? redirect(PERMISSIONS_CONFIG.redirects.authenticated)
+            : supabaseResponse
+    }
+
+    // Usuários logados não deveriam acessar as rotas de login
+    if (matchesRoute(PERMISSIONS_CONFIG.authRoutes)) {
+        if (user) {
+            return redirect(PERMISSIONS_CONFIG.redirects.authenticated)
+        }
+        return supabaseResponse
+    }
+
+    // Rotas públicas
+    if (matchesRoute(PERMISSIONS_CONFIG.publicRoutes)) {
+        return supabaseResponse
+    }
+
+    // Usuário não logado tentando acessar alguma rota não pública e não auth
+    if (!user) {
+        return redirect(PERMISSIONS_CONFIG.redirects.unauthenticated, pathname)
+    }
+
+    const { data: profile, error: profileError } = await supabase
+        .from('users')
+        .select('role_id')
+        .eq('id', user.id)
+        .single()
+
+    // Houve algum erro ao determinar papel do usuário
+    if (!profile || profileError) {
+        console.error(profileError)
+        return redirect(PERMISSIONS_CONFIG.redirects.unauthenticated, pathname)
+    }
+
+    // Usuário logado tenta acessar rotas que ele não tem permissão
+    const restrictedRoute = Object.keys(
+        PERMISSIONS_CONFIG.restrictedRoutes
+    ).find((route) => pathname === route || pathname.startsWith(`${route}/`))
+
+    if (restrictedRoute) {
+        if (
+            !PERMISSIONS_CONFIG.restrictedRoutes[restrictedRoute].includes(
+                profile.role_id
+            )
+        ) {
+            return redirect(PERMISSIONS_CONFIG.redirects.unauthorized)
+        }
+        return supabaseResponse
     }
 
     // IMPORTANT: You *must* return the supabaseResponse object as it is.
