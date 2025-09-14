8d69fccace8b73f0863ac7a3ae8c5d707c099ed8
Cristina Souza <cristinars@dcc.ufrj.br>
2025-09-14 10:46:10 -0300

    fix: fixing auth button props

--- src/components/auth-button.tsx
+++ src/components/auth-button.tsx
@@ -17,10 +17,10 @@ export async function AuthButton() {
         </div>
     ) : (
         <div className="flex gap-2">
-            <Button asChild size="sm" variant={'outline'}>
+            <Button size="sm" variant={'outline'}>
                 <Link href="/auth/login">Entrar</Link>
             </Button>
-            <Button asChild size="sm" variant={'default'}>
+            <Button size="sm" variant={'default'}>
                 <Link href="/auth/sign-up">Criar nova conta</Link>
             </Button>
         </div>
